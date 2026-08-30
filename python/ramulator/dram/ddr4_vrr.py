6830adcb11cd90f6cce3d587f0f51623e75c3516
Haocong Luo <haocong.luo@safari.ethz.ch>
2026-08-30 04:59:15 +0000

    centralize DDR4_VRR timing ownership

--- python/ramulator/dram/ddr4_vrr.py
+++ python/ramulator/dram/ddr4_vrr.py
@@ -11,17 +11,21 @@ class DDR4_VRR(DDR4):
 
     timing_params = DDR4.timing_params + ["nVRR"]
     timing_constraints = DDR4.timing_constraints + [
-        TimingConstraint(level="Bank", preceding=["VRR"], following=["ACT"], latency="nVRR"),
+        TimingConstraint(level="Bank", preceding=["VRR"], following=["ACT", "VRR"], latency="nVRR"),
         TimingConstraint(level="Bank", preceding=["ACT"], following=["VRR"], latency="nRC"),
         TimingConstraint(level="Rank", preceding=["PREpb", "PREab"], following=["VRR"], latency="nRP"),
     ]
 
+    @classmethod
+    def resolve_secondary_timings(cls, timing_dict, org_dict):
+        super().resolve_secondary_timings(timing_dict, org_dict)
+        timing_dict["nVRR"] = cls._resolve_nVRR(timing_dict["tCK_ps"])
 
-# Inherit all DDR4 presets, adding nVRR timing
+    @staticmethod
+    def _resolve_nVRR(tCK_ps):
+        return math.ceil(280_000 / tCK_ps)  # Ramulator guesstimate
+
+
+# Inherit all DDR4 presets; nVRR is resolved from tCK_ps.
 DDR4_VRR.org_presets = DDR4.org_presets
-DDR4_VRR.timing_presets = {}
-for _name, _timings in DDR4.timing_presets.items():
-    _vrr_timings = dict(_timings)
-    # nVRR ~ 70ns * RH_radius(2) * 2 = 280ns, converted to cycles
-    _vrr_timings["nVRR"] = math.ceil(280_000 / _timings["tCK_ps"])
-    DDR4_VRR.timing_presets[_name] = _vrr_timings
+DDR4_VRR.timing_presets = DDR4.timing_presets
